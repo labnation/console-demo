c2d3ee2c2f488540705210de6f206c8d174625df
Jasper van Bourgognie <van.bourgognie@gmail.com>
2015-06-17 16:07:14 +0200

    initial commit

---
+++ Conscople/Main.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Mono.Terminal;
+
+namespace ConsoleTest
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			Application.Init (false);
+			Dialog d = new Dialog (40, 8, "Hello");
+
+			d.Add (new Label (0, 0, "Hello World"));
+			Application.Run (d);
+		}
+	}
+}
---
+++ bootstrap.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -e
+
+if [[ -z "$1" ]]; then
+  echo Usage:
+  echo 
+  echo "$0 <Platform>"
+  echo 
+  echo "Where <Platform> can be Linux|MacOS|Windows"
+  exit -1
+fi
+
+HOST=`uname`
+if [[ x$HOST != xMINGW* ]]; then
+	CMD_PREFIX="mono ./"
+else
+	CMD_PREFIX="./"
+fi
+echo Current directory: `pwd`
+
+${CMD_PREFIX}Protobuild.exe --generate $1 
+sed -i~ -e "s/<ProjectTypeGuids>.*<\/ProjectTypeGuids>//" Conscople/Conscople.$1.csproj
+sed -i~ -e "s/\(<UseSGen>False<\/UseSGen>\)/\1<Externalconsole>true<\/Externalconsole>/" Conscople/Conscople.$1.csproj
+
+${CMD_PREFIX}.nuget/NuGet.exe restore Conscople.$1.sln
+
+cd mono-curses
+make
+cd -
