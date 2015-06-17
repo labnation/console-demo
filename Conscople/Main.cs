33ac20e5e4319d3480b9432d7d7da63b39ac6df5
Jasper van Bourgognie <van.bourgognie@gmail.com>
2015-06-17 17:18:55 +0200

    Basic...

--- Conscople/Main.cs
+++ Conscople/Main.cs
@@ -1,17 +1,13 @@
 ﻿using System;
-using Mono.Terminal;
 
-namespace ConsoleTest
+namespace Conscople
 {
 	class MainClass
 	{
+
 		public static void Main(string[] args)
 		{
-			Application.Init (false);
-			Dialog d = new Dialog (40, 8, "Hello");
-
-			d.Add (new Label (0, 0, "Hello World"));
-			Application.Run (d);
+			ScopeConsoleInterface i = new ScopeConsoleInterface ();
 		}
 	}
 }
