fe38ca78f1bd236dd942590f9b10d141f075be00
Jasper van Bourgognie <van.bourgognie@gmail.com>
2015-06-18 12:24:03 +0200

    Update bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -18,6 +18,8 @@ else
 fi
 echo Current directory: `pwd`
 
+git submodule init
+git submodule update 
 ${CMD_PREFIX}Protobuild.exe --generate $1 
 sed -i~ -e "s/<ProjectTypeGuids>.*<\/ProjectTypeGuids>//" Conscople/Conscople.$1.csproj
 sed -i~ -e "s/\(<UseSGen>False<\/UseSGen>\)/\1<Externalconsole>true<\/Externalconsole>/" Conscople/Conscople.$1.csproj
@@ -25,5 +27,6 @@ sed -i~ -e "s/\(<UseSGen>False<\/UseSGen>\)/\1<Externalconsole>true<\/Externalco
 ${CMD_PREFIX}.nuget/NuGet.exe restore Conscople.$1.sln
 
 cd mono-curses
+./configure
 make
 cd -
